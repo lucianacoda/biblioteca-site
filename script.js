17ba7f9ecf046f5ccb78fd47c7a402703607a395
lucianacoda <lcsg23@hotmail.com>
2025-06-25 18:46:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function saudacao() {
+    alert("Seja bem-vindo(a) à nossa Biblioteca Escolar! 📚");
+}
+
+function toggleMenu() {
+    const menuLinks = document.getElementById('menuLinks');
+    menuLinks.classList.toggle('active');
+}
